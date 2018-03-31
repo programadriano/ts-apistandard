5ddecc0f9abd2b5c2fb64e9ca465796fc6c06c8e
tadriano <tadriano@tadriano.local>
2018-03-31 11:40:31 -0300

    subindo v1

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-let port = process.env.PORT || '3030';
+let port = process.env.PORT || '3000';
 
 app.app.listen(port, function () {
     console.log(`server running in" + ${port}`);
